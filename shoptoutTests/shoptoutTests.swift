d72e880d6802c88e5686e3975898b822f745e731
tarq <ctarquini@launchdarkly.com>
2025-07-08 20:15:46 -0400

    Initial Commit

---
+++ shoptoutTests/shoptoutTests.swift
@@ -0,0 +1,17 @@
+//
+//  shoptoutTests.swift
+//  shoptoutTests
+//
+//  Created by Christopher Tarquini on 7/8/25.
+//
+
+import Testing
+@testable import shoptout
+
+struct shoptoutTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
